fe5c06ecf9bbdab3dab321522f66dc0ec3bf181f
xavierthomas22 <xavier.thomas@sharechat.co>
2022-11-17 08:14:17 +0000

    .

--- src/pages/4_zero_shot.py
+++ src/pages/4_zero_shot.py
@@ -13,19 +13,18 @@ url = "https://farm4.staticflickr.com/3445/3262471985_ed886bf61a_z.jpg"
 image = download(url)
 
 pil_img = Image.open(image)
-# # display(pil_img)
-
 st.image(pil_img)
+st.image(image, caption='Example Image')
 # # exit()
 
-# from autogluon.multimodal import MultiModalPredictor
+from autogluon.multimodal import MultiModalPredictor
 
-# class_texts = ['This is a Husky', 'This is a Golden Retriever', 'This is a German Sheperd', 'This is a Samoyed.']
+class_texts = ['This is a Husky', 'This is a Golden Retriever', 'This is a German Sheperd', 'This is a Samoyed.']
 
-# predictor = MultiModalPredictor(pipeline="zero_shot_image_classification")
-# prob = predictor.predict_proba({"image": [image]}, {"text": class_texts})
-# prob_tensor = torch.tensor(prob[0])
-# print("Label probs:", prob_tensor)
+predictor = MultiModalPredictor(pipeline="zero_shot_image_classification")
+prob = predictor.predict_proba({"image": [image]}, {"text": class_texts})
+prob_tensor = torch.tensor(prob[0])
+print("Label probs:", prob_tensor)
 
-# values, indices = prob_tensor.topk(3)
-# print(indices)
+values, indices = prob_tensor.topk(3)
+print(indices)
