43c553319e6e6f6648339d16da3d09cdd32dfb3c
xavierthomas22 <xavier.thomas@sharechat.co>
2022-11-17 08:00:18 +0000

    .

--- src/pages/4_zero_shot.py
+++ src/pages/4_zero_shot.py
@@ -6,13 +6,13 @@ st.write("# Welcome to CUTUMB ZERO SHOT TEMP 2! 👋")
 # print('hello')
 
 
-# from IPython.display import Image, display
+from IPython.display import Image, display
 from autogluon.multimodal import download
 
 url = "https://farm4.staticflickr.com/3445/3262471985_ed886bf61a_z.jpg"
 image = download(url)
 
-# pil_img = Image(filename=image)
+pil_img = Image(filename=image)
 # # display(pil_img)
 
 st.image(pil_img)
